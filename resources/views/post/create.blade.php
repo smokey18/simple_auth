b723d06c7298f6d8116c4ac37a74a9274db36bbd
Smokey <kmboplays@gmail.com>
2021-10-18 18:40:25 +0500

    added live upload preview on adding new image

--- resources/views/post/create.blade.php
+++ resources/views/post/create.blade.php
@@ -1,6 +1,12 @@
 @extends('layouts.header')
 @section('title', 'Add New Post')
 @section('content')
+    <style>
+        .thumb{
+            margin: 10px 5px 0 0;
+            width: 300px;
+        }
+    </style>
     <div class="container">
         <div class="row">
             <div class="col-md-6 col-md-offset-3" style="margin-top:50px">
@@ -29,8 +35,9 @@
 
                     <div class="form-group">
                         <label for="image">Post Image</label>
-                        <input type="file" class="form-control" name="image[]" multiple>
+                        <input type="file" onchange="loadPreview(this)" name="image[]" multiple>
                         <span style="color: red">@error('image'){{ $message }} @enderror</span>
+                        <div id="thumb-output"></div>
                     </div>
 
                     <div class="form-group">
@@ -40,4 +47,22 @@
             </div>
         </div>
     </div>
+
+    <script>
+        function loadPreview(input){
+            var data = $(input)[0].files; //this file data
+            $.each(data, function(index, file){
+                if(/(\.|\/)(gif|jpe?g|png)$/i.test(file.type)){
+                    var fRead = new FileReader();
+                    fRead.onload = (function(file){
+                        return function(e) {
+                            var img = $('<img/>').addClass('thumb').attr('src', e.target.result); //create image thumb element
+                            $('#thumb-output').append(img);
+                        };
+                    })(file);
+                    fRead.readAsDataURL(file);
+                }
+            });
+        }
+     </script>
 @endsection
